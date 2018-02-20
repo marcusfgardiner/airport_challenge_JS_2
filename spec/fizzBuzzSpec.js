839fbc6f6dcc508b4897a317dd95de031075fc1e
Hannah Lillis <hannahamanda16@gmail.com>
2018-02-20 15:27:38 +0000

    Finish Fizzbuzz and print numbers to console

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -39,4 +39,20 @@ describe('Javabuzz', function() {
 
   });
 
+  describe('when playing, says', function() {
+
+    it('"Java" when a number is divisible by 3', function() {
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
+
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(javabuzz.says(5)).toEqual("Buzz");
+    });
+
+    it('"JavaBuzz" when a number is divisible by 15', function() {
+      expect(javabuzz.says(15)).toEqual("JavaBuzz");
+    });
+
+  });
+
 });
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,7 +2,7 @@ function Javabuzz() {};
 
 Javabuzz.prototype._isDivisibleBy = function(number, divider) {
   return ( number % divider === 0 );
-}
+};
 
 Javabuzz.prototype.isDivisibleByThree = function(number) {
   return this._isDivisibleBy(number, 3);
@@ -16,4 +16,21 @@ Javabuzz.prototype.isDivisibleByFifteen = function(number) {
   return this._isDivisibleBy(number, 15);
 };
 
-console.log(4 % 5 === 0 )
+Javabuzz.prototype.says = function(number) {
+  if (this.isDivisibleByFifteen(number)) {
+    return "JavaBuzz";
+  }
+  if (this.isDivisibleByThree(number)) {
+    return "Java";
+  }
+  if (this.isDivisibleByFive(number)) {
+    return "Buzz";
+  }
+  return number;
+};
+
+javabuzz = new Javabuzz();
+
+for (var i = 1; i <= 100; i++) {
+  console.log(javabuzz.says(i));
+}
