35b3e4affb33c154ca248da6605585cb79fe782d
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-21 17:03:04 +0000

    refactored code to fulfill single responsibility

--- src/airport.js
+++ src/airport.js
@@ -3,26 +3,24 @@
 var Airport = function(capacity) {
   this.hangar = [];
   this.weather = new Weather();
-  this.capacity = capacity || 20
+  this.capacity = capacity || 20;
 };
 
 Airport.prototype.land = function(plane) {
-  if (this.hangar.length >= this.capacity) {
-    throw 'Too many planes!'
-  }
-  else if (this.weather.isStormy()) {
-    throw 'It is stormy out there!';
-  }
-  else {
+  this._fullError();
+  this._stormyError();
     return this.hangar.push(plane);
-  }
 };
 
 Airport.prototype.takeOff = function(plane) {
-  if (this.weather.isStormy()) {
-    throw 'It is stormy out there!';
-  }
-  else {
+    this._stormyError();
     return this.hangar.pop(plane);
-  }
+};
+
+Airport.prototype._stormyError = function () {
+    if (this.weather.isStormy()) throw 'It is stormy out there!';
+};
+
+Airport.prototype._fullError = function () {
+  if (this.hangar.length >= this.capacity) throw 'Too many planes!';
 };
